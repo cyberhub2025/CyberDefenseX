e887fb670617670bea2fbee9c1216a05a22cc30d
Shuvojit Samanta <shuvojitsamanta15@gmail.com>
2026-06-12 10:55:49 +0530

    Updated Scroll

--- frontend/src/hooks/useLenisScroll.js
+++ frontend/src/hooks/useLenisScroll.js
@@ -11,8 +11,8 @@ export const useLenisScroll = () => {
       gestureDirection: 'vertical', // vertical, horizontal, both
       smooth: true,
       mouseMultiplier: 1,
-      smoothTouch: false,
-      touchMultiplier: 2,
+      smoothTouch: true,
+      touchMultiplier: 1,
       infinite: false,
     });
 
